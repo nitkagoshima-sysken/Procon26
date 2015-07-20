04317b3cb222bb1a35656bc69aeceecce484314b
ryo33 <ryo12redstone@gmail.com>
2015-07-20 16:32:44 +0900

    Make functions which returned a Stone to return a pointer of Stone

--- procon26_module.cpp
+++ procon26_module.cpp
@@ -23,19 +23,19 @@ const Stone *EMPTY_STONE = new Stone;
 void showStone(Stone *);
 int countBit(unsigned char);// Count-Bit
 int countBitOfStone(Stone *stone);// Count-Bit-Of-Stone
-Stone quarryStone(Board *, int, int); // Cut-Stone
-Stone shift_up(Stone);		// Shift-Up
-Stone shift_down(Stone);	// Shift-Down
-Stone shift_right(Stone);	// Shift-Right
-Stone shift_left(Stone);	// Shift-Left
-Stone turn_90(Stone);		// Turn-90  (deg) 
-Stone turn_180(Stone);		// Turn-180 (deg)
-Stone turn_270(Stone);		// Turn-270 (deg)
-Stone reverce(Stone);		// Reverce
-Stone NOT(Stone);			// Logic-NOT 
-Stone AND(Stone, Stone);	// Logic-AND
-Stone  OR(Stone, Stone);	// Logic-OR
-Stone XOR(Stone, Stone);	// Logic-XOR
+Stone *quarryStone(Board *, int, int); // Cut-Stone
+Stone *shift_up(Stone *);		// Shift-Up
+Stone *shift_down(Stone *);	// Shift-Down
+Stone *shift_right(Stone *);	// Shift-Right
+Stone *shift_left(Stone *);	// Shift-Left
+Stone *turn_90(Stone *);		// Turn-90  (deg)
+Stone *turn_180(Stone *);		// Turn-180 (deg)
+Stone *turn_270(Stone *);		// Turn-270 (deg)
+Stone *reverce(Stone *);		// Reverce
+Stone *NOT(Stone *);			// Logic-NOT
+Stone *AND(Stone *, Stone *);	// Logic-AND
+Stone *OR(Stone *, Stone *);	// Logic-OR
+Stone *XOR(Stone *, Stone *);	// Logic-XOR
 bool isEmptyStone(Stone *);
 
 /* Implementation */
@@ -69,62 +69,64 @@ int countBitOfStone(Stone *stone)
 	return sum;
 }
 
-Stone quarryStone(Board *board, int x, int y)
+Stone *quarryStone(Board *board, int x, int y)
 {
-	Stone Cut;
-	
+	Stone *quarried = new Stone;
 	for(int i = 0; i < 8; i++)
 	{
-		Cut.zuku[i] = board->block[x / 8 + y + (i * 4)] << (x % 8) | board->block[x / 8 + y + (i * 4) + 1] >> (8 - (x % 8));
+		quarried->zuku[i] = board->block[x / 8 + y + (i * 4)] << (x % 8) | board->block[x / 8 + y + (i * 4) + 1] >> (8 - (x % 8));
 	}
-	
-	return Cut;
+	return quarried;
 }
 
-Stone shift_up(Stone stone)
+Stone *shift_up(Stone *stone)
 {
+	Stone *dist = new Stone;
 	for (int i = 0; i < STONE_SIZE - 1; i++)
 	{
-		stone.zuku[i] = stone.zuku[i + 1];
+		dist->zuku[i] = stone->zuku[i + 1];
 	}
-	stone.zuku[STONE_SIZE - 1] = 0;
+	dist->zuku[STONE_SIZE - 1] = 0;
 	
-	return stone;
+	return dist;
 }
 
-Stone shift_down(Stone stone)
+Stone *shift_down(Stone *stone)
 {
+	Stone *dist = new Stone;
 	for (int i = STONE_SIZE - 1; i > 0; i--)
 	{
-		stone.zuku[i] = stone.zuku[i - 1];
+		dist->zuku[i] = stone->zuku[i - 1];
 	}
-	stone.zuku[0] = 0;
+	dist->zuku[0] = 0;
 	
-	return stone;
+	return dist;
 }
 
-Stone shift_right(Stone stone)
+Stone *shift_right(Stone *stone)
 {
+	Stone *dist = new Stone;
 	for (int i = 0; i < STONE_SIZE; i++)
 	{
-		stone.zuku[i] = (stone.zuku[i] >> 1);
+		dist->zuku[i] = (stone->zuku[i] >> 1);
 	}
-	return stone;
+	return dist;
 }
 
-Stone shift_left(Stone stone)
+Stone *shift_left(Stone *stone)
 {
+	Stone *dist = new Stone;
 	for (int i = 0; i < STONE_SIZE; i++)
 	{
-		stone.zuku[i] = (stone.zuku[i] << 1);
+		dist->zuku[i] = (stone->zuku[i] << 1);
 	}
-	return stone;
+	return dist;
 }
 
-Stone turn90(Stone stone)
+Stone *turn90(Stone *stone)
 {
-	Stone dist;
-	for (int i = 0; i < STONE_SIZE; i++) dist.zuku[i] = 0;
+	Stone *dist = new Stone;
+	for (int i = 0; i < STONE_SIZE; i++) dist->zuku[i] = 0;
 	
 	for (int i = 0; i < 64; i++)
 	{
@@ -134,19 +136,19 @@ Stone turn90(Stone stone)
 		int nx = STONE_SIZE - 1 - y;
 		int ny = x;
 		
-		if ((stone.zuku[y] << x) & 128)
+		if ((stone->zuku[y] << x) & 128)
 		{
-			dist.zuku[ny] += 128 >> nx;
+			dist->zuku[ny] += 128 >> nx;
 		}
 	}
 	
 	return dist;
 }
 
-Stone turn180(Stone stone)
+Stone *turn180(Stone *stone)
 {
-	Stone dist;
-	for (int i = 0; i < STONE_SIZE; i++) dist.zuku[i] = 0;
+	Stone *dist = new Stone;
+	for (int i = 0; i < STONE_SIZE; i++) dist->zuku[i] = 0;
 	
 	for (int i = 0; i < 64; i++)
 	{
@@ -156,19 +158,19 @@ Stone turn180(Stone stone)
 		int nx = STONE_SIZE - 1 - x;
 		int ny = STONE_SIZE - 1 - y;
 		
-		if ((stone.zuku[y] << x) & 128)
+		if ((stone->zuku[y] << x) & 128)
 		{
-			dist.zuku[ny] += 128 >> nx;
+			dist->zuku[ny] += 128 >> nx;
 		}
 	}
 	
 	return dist;
 }
 
-Stone turn270(Stone stone)
+Stone *turn270(Stone *stone)
 {
-	Stone dist;
-	for (int i = 0; i < STONE_SIZE; i++) dist.zuku[i] = 0;
+	Stone *dist = new Stone;
+	for (int i = 0; i < STONE_SIZE; i++) dist->zuku[i] = 0;
 	
 	for (int i = 0; i < 64; i++)
 	{
@@ -178,19 +180,19 @@ Stone turn270(Stone stone)
 		int nx = y;
 		int ny = STONE_SIZE - 1 - x;
 		
-		if ((stone.zuku[y] << x) & 128)
+		if ((stone->zuku[y] << x) & 128)
 		{
-			dist.zuku[ny] += 128 >> nx;
+			dist->zuku[ny] += 128 >> nx;
 		}
 	}
 	
 	return dist;
 }
 
-Stone reverce(Stone stone)
+Stone *reverce(Stone *stone)
 {
-	Stone dist;
-	for (int i = 0; i < STONE_SIZE; i++) dist.zuku[i] = 0;
+	Stone *dist = new Stone;
+	for (int i = 0; i < STONE_SIZE; i++) dist->zuku[i] = 0;
 
 	for (int i = 0; i < 64; i++)
 	{
@@ -200,49 +202,53 @@ Stone reverce(Stone stone)
 		int nx = STONE_SIZE - 1 - x;
 		int ny = y;
 
-		if ((stone.zuku[y] << x) & 128)
+		if ((stone->zuku[y] << x) & 128)
 		{
-			dist.zuku[ny] += 128 >> nx;
+			dist->zuku[ny] += 128 >> nx;
 		}
 	}
 
 	return dist;
 }
 
-Stone NOT(Stone stone)
+Stone *NOT(Stone *stone)
 {
+	Stone *resultStone = new Stone;
 	for(int i = 0; i < STONE_SIZE; i++)
 	{
-		stone.zuku[i] = ~stone.zuku[i];
+		resultStone->zuku[i] = ~stone->zuku[i];
 	}
-	return stone;
+	return resultStone;
 }
 
-Stone AND(Stone stone1, Stone stone2)
+Stone *AND(Stone *stone1, Stone *stone2)
 {
+	Stone *resultStone = new Stone;
 	for(int i = 0; i < STONE_SIZE; i++)
 	{
-		stone1.zuku[i] = stone1.zuku[i] & stone2.zuku[i];
+		resultStone->zuku[i] = stone1->zuku[i] & stone2->zuku[i];
 	}
-	return stone1;
+	return resultStone;
 }
 
-Stone OR(Stone stone1, Stone stone2)
+Stone *OR(Stone *stone1, Stone *stone2)
 {
+	Stone *resultStone = new Stone;
 	for(int i = 0; i < STONE_SIZE; i++)
 	{
-		stone1.zuku[i] = stone1.zuku[i] | stone2.zuku[i];
+		resultStone->zuku[i] = stone1->zuku[i] | stone2->zuku[i];
 	}
-	return stone1;
+	return resultStone;
 }
 
-Stone XOR(Stone stone1, Stone stone2)
+Stone *XOR(Stone *stone1, Stone *stone2)
 {
+	Stone *resultStone = new Stone;
 	for(int i = 0; i < STONE_SIZE; i++)
 	{
-		stone1.zuku[i] = stone1.zuku[i] ^ stone2.zuku[i];
+		resultStone->zuku[i] = stone1->zuku[i] ^ stone2->zuku[i];
 	}
-	return stone1;
+	return resultStone;
 }
 
 bool isEmptyStone(Stone *stone)
