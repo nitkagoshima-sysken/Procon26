62801be6b697905f4968b818a457b1e8c7bf7c42
ryo33 <ryo12redstone@gmail.com>
2015-07-20 21:31:42 +0900

    Remove test.py from test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,15 @@
-python test/auto_test/test.py $@
+"cmake_minimum_required(VERSION 3.0)
+set(GTEST_ROOT /usr/local/lib)
+include_directories(/usr/local/include)" > test/CMakeLists.txt
+for file in "$@"
+do
+    "add_executable(${file}_test ${file}_test.cpp)
+    target_link_libraries(${file}_test
+    pthread
+    ${GTEST_ROOT}/libgtest.a
+    ${GTEST_ROOT}/libgtest_main.a
+    )" >> test/CMakeLists.txt
+done
 mkdir test/build
 cd test/build
 cmake ..
