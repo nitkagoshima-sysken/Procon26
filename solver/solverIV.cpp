ec135057972ded4aa88054cf40ab1b86eb135a35
ryo33 <ryo12redstone@gmail.com>
2015-08-22 02:33:15 +0900

    Add solverIV.cpp

---
+++ solver/solverIV.cpp
@@ -0,0 +1,59 @@
+#include "../procon26_module.hpp"
+#include "../procon26_modlib.hpp"
+#include "../procon26_modio.hpp"
+
+#define SOLVER_FOR for(int x = -(STONE_SIZE - 1); x < BOARD_SIZE + (STONE_SIZE - 1); x ++) for(int y = -(STONE_SIZE - 1); y < BOARD_SIZE + (STONE_SIZE - 1); y ++)
+
+class SolverIV{
+
+    public:
+        static Answers *solve(Problem &problem){
+            std::vector<std::vector<Stone *> > states, picked;
+            convertStonesToStates(problem.stones, problem.num, states); // TODO
+            std::vector<int> zukus;
+            countZukusOfStones(problem.stones, problem.num, zukus); // TODO
+            Answers *answer = new Answers();
+            StonePicker *stonePicker = new StonePicker(states, zukus, BOARD_SIZE * BOARD_SIZE - countBitOfBoard(problem.board));
+            while(true){
+                stonePicker->getNext(picked);
+                if(picked.size() == 0){
+                    // TODO free used memories
+                    return NULL;
+                }
+                Answers *result = solveInternal(answer, boardChecker, problem.board, cloneBoard(EMPTY_BOARD), picked, 0, true);
+                if(result != NULL){
+                    // TODO free used memories
+                    return result;
+                }
+            }
+        }
+
+    private:
+        static Answers *solveInternal(
+                Answers *answer,
+                BoardBoolean *boardChecker,
+                Board *masterBoard,
+                Board *stonesBoard,
+                std::vector<std::vector<Stone *> > &states,
+                int depth,
+                bool first=false){
+            SOLVER_FOR if(boardChecker->check(x, y)) for(int i = 0; i < states[depth].size(); i ++){
+                if(canPlace(masterBoard, stonesBoard, states[depth][i], x, y, first)){
+                    Answers *placedAnswer = answer->place(states[depth][i], x, y); // TODO
+                    if(depth == states.size() - 1){
+                        return placedAnswer;
+                    }
+                    BoardBoolean *placedBoardChecker = boardChecker->place(x, y); // TODO
+                    Board *placedMasterBoard = placeStone(masterBoard, states[depth][i], x, y);
+                    Board *placedStonesBoard = placeStone(stonesBoard, states[depth][i], x, y);
+                    Answers *result = solveInternal(placedAnswer, placedBoardChecker, placedMasterBoard, placedStonesBoard, states, depth + 1);
+                    // TODO free used memories
+                    if(result != NULL){
+                        return result;
+                    }
+                }
+            }
+            return NULL;
+        }
+
+};
