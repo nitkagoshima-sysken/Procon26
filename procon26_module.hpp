8eff250fbe5cdf3cf889f1785616587305e403d6
kaito3030 <ikekai3030@gmail.com>
2015-08-24 01:43:55 +0900

    Fix a bug

--- procon26_module.hpp
+++ procon26_module.hpp
@@ -46,7 +46,7 @@ struct Answers
 class BoardBoolean
 {
 	public:
-		bool boolean[BOARD_SIZE + (STONE_SIZE - 1) * 2][BOARD_SIZE + (STONE_SIZE - 1) * 2];
+		bool boolean[BOARD_SIZE + (STONE_SIZE - 1) * 2 + 1][BOARD_SIZE + (STONE_SIZE - 1) * 2 + 1];
 		BoardBoolean();
 		BoardBoolean *place(int x, int y);	//石を置いた座標を入力し、bool型の2次元配列に反映する
 		int check(int x, int y);	//ボードの座標を入力し、その座標に石が置けるかどうかを確認する
